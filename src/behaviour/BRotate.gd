54063679022c0e30eb27bf9059e998b598004f30
Amanda Severo <miwafoxl@proton.me>
2026-08-24 11:31:56 -0300

    refactor: remove behaviour preprocessing entirely; add behaviour flags

--- src/behaviour/BRotate.gd
+++ src/behaviour/BRotate.gd
@@ -1,16 +1,17 @@
 class_name BRotate extends Behaviour
 
-@export var amount: float = 0.05
-
-var rotating: Sprite2D
+var amount: float = 0.05
+var rotating: Sprite2D = actor
+var multiplier: int = 1
 
 func init() -> void:
+	amount = flags.get("amount", amount)
+	multiplier = -1 if flags.has("reverse") else 1
 	if rotating == null:
 		rotating = actor
-
+	
 func condition(_delta: float) -> bool:
 	return true
 
 func action(_delta: float) -> void:
-	rotating.rotate(-amount)
-	event.emit("sound.rotating")
+	rotating.rotate(amount * multiplier)
